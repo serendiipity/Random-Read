929c23f628f17b3595e7fe636f2440b61f3ce074
serendiipity <imane.chk@gmail.com>
2019-06-04 23:05:43 +0000

    initial commit

---
+++ Random Read/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Random Read
+//
+//  Created by imane on 6/4/19.
+//  Copyright © 2019 imane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
